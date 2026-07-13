39584cc68e6e76511056d2d4588e7a0ab360a6dd
zahid-01 <dev.zahidhk@gmail.com>
2026-07-13 22:11:24 +0530

    feat(hermes): added new hermes APIS

--- hpService.js
+++ hpService.js
@@ -1,42 +1,20 @@
 const axios = require("axios");
 const { getAccessToken } = require("./tokenManager");
 
-async function callHermes(payload) {
+async function callHermes(endpoint, payload) {
   const token = await getAccessToken();
+  const baseUrl =
+    process.env.HP_BASE_URL ||
+    "https://hpit-gw.hpcloud.hp.com/generic-router/api/hermes";
 
-  const response = await axios.post(
-    process.env.HP_HERMES_URL,
-    payload,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const response = await axios.post(`${baseUrl}/${endpoint}`, payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
 
   return response.data;
 }
 
-async function callCatalogItems(payload) {
-  const token = await getAccessToken();
-
-  const response = await axios.post(
-    process.env.HP_CATALOG_ITEMS_URL ||
-      "https://hpit-gw.hpcloud.hp.com/generic-router/api/hermes/catalogitems",
-    payload,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-
-  return response.data;
-}
-
-module.exports = {
-  callHermes,
-  callCatalogItems,
-};
+module.exports = { callHermes };
--- routes/hp.js
+++ routes/hp.js
@@ -1,116 +1,48 @@
 const express = require("express");
-
 const router = express.Router();
+const { getAccessToken, refreshAccessToken } = require("../tokenManager");
+const { callHermes } = require("../hpService");
 
-const {
-  getAccessToken,
-  refreshAccessToken,
-} = require("../tokenManager");
-
-const {
-  callHermes,
-  callCatalogItems,
-} = require("../hpService");
-
-/**
- * Returns current token
- */
 router.get("/token", async (req, res) => {
   try {
-    const token = await getAccessToken();
-
-    res.json({
-      access_token: token,
-    });
+    res.json({ access_token: await getAccessToken() });
   } catch (err) {
-    res.status(500).json(err.response?.data || {
-      message: err.message,
-    });
+    res.status(500).json(err.response?.data || { message: err.message });
   }
 });
 
-/**
- * Force refresh token
- */
 router.post("/refresh-token", async (req, res) => {
   try {
-    const token = await refreshAccessToken();
-
-    res.json({
-      access_token: token,
-    });
+    res.json({ access_token: await refreshAccessToken() });
   } catch (err) {
-    res.status(500).json(err.response?.data || {
-      message: err.message,
-    });
+    res.status(500).json(err.response?.data || { message: err.message });
   }
 });
 
-/**
- * Proxy to HP Hermes
- */
-router.post("/hermes", async (req, res) => {
-  try {
-    const result = await callHermes(req.body);
+const hermesEndpoints = [
+  "productcontent",
+  "companions",
+  "images",
+  "richmedia",
+  "itempartnerdocs",
+  "hierarchy",
+  "plc",
+  "catalogitems",
+  "catalogfacetfilters",
+  "itemsbyfacetvalues",
+];
 
-    res.json(result);
-  } catch (err) {
-    res.status(err.response?.status || 500).json(
-      err.response?.data || {
-        message: err.message,
-      }
-    );
-  }
+hermesEndpoints.forEach((endpoint) => {
+  router.post(`/${endpoint}`, async (req, res) => {
+    try {
+      const result = await callHermes(endpoint, req.body);
+      res.json(result);
+    } catch (err) {
+      res.status(err.response?.status || 500).json(
+        err.response?.data || { message: err.message }
+      );
+    }
+  });
 });
-
-/**
- * Proxy to HP Hermes catalog items
- */
-router.post("/catalogitems", async (req, res) => {
-  try {
-    const result = await callCatalogItems(req.body);
-
-    res.json(result);
-  } catch (err) {
-    res.status(err.response?.status || 500).json(
-      err.response?.data || {
-        message: err.message,
-      }
-    );
-  }
-});
-
-// const axios = require("axios");
-// const { getAccessToken } = require("./tokenManager");
-//
-// async function callHermes(payload) {
-//   const token = await getAccessToken();
-//
-//   const response = await axios.post(
-//       process.env.HP_HERMES_URL,
-//       {
-//         "sku": [
-//           "715A5A"
-//         ],
-//         "countryCode": "IN",
-//         "languageCode": "EN",
-//         "layoutName": "ALL-Specs",
-//         "requestor": "DSKASHMIR-PRO"
-//       },
-//       {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//           "Content-Type": "application/json",
-//         },
-//       }
-//   );
-//
-//   return response.data;
-// }
-//
-// module.exports = {
-//   callHermes,
-// };
-
 
 module.exports = router;
